30a2118e7aa04ff3dee8ea6fb65f71ba595acdfd
AlexMaz99 <ola_m10@onet.pl>
2020-01-19 23:46:44 +0100

    changes2

--- constraints.sql
+++ constraints.sql
@@ -1,3 +1,8 @@
 ALTER TABLE dbo.Employees ADD CONSTRAINT LikeEmail CHECK (Email LIKE'%_@%._%')
 ALTER TABLE dbo.DayPrices ADD CONSTRAINT SDiscount CHECK (StudentDiscount>=0 AND StudentDiscount<=1)
 ALTER TABLE dbo.Days ADD CONSTRAINT PositiveParticipants CHECK (MaxParticipants > 0)
+ALTER TABLE dbo.Days ADD CONSTRAINT PositiveDays CHECK (DayNumber > 0)
+ALTER TABLE dbo.Workshops ADD CONSTRAINT PositiveParticipants2 CHECK (MaxParticipants > 0)
+ALTER TABLE dbo.Workshops ADD CONSTRAINT NotNegativePrice CHECK (Price >= 0)
+ALTER TABLE dbo.Payments ADD CONSTRAINT NotNegativeTotal CHECK (Total >= 0)
+ALTER TABLE dbo.ConferenceBookings ADD CONSTRAINT CheckStatus CHECK (Status = -1 OR Status = 0 OR Status = 1)
