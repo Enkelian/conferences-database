e75e839acce79f0ac0c422dc077cfd4af9d1fe32
AlexMaz99 <ola_m10@onet.pl>
2020-01-19 23:50:30 +0100

    changes

--- constraints.sql
+++ constraints.sql
@@ -6,3 +6,8 @@ ALTER TABLE dbo.Workshops ADD CONSTRAINT PositiveParticipants2 CHECK (MaxPartici
 ALTER TABLE dbo.Workshops ADD CONSTRAINT NotNegativePrice CHECK (Price >= 0)
 ALTER TABLE dbo.Payments ADD CONSTRAINT NotNegativeTotal CHECK (Total >= 0)
 ALTER TABLE dbo.ConferenceBookings ADD CONSTRAINT CheckStatus CHECK (Status = -1 OR Status = 0 OR Status = 1)
+ALTER TABLE dbo.DayBookings ADD CONSTRAINT CheckStatusDB CHECK (Status = -1 OR Status = 0 OR Status = 1)
+ALTER TABLE dbo.DayBookings ADD CONSTRAINT PositiveParticipantsDB CHECK (NumberOfParticipants > 0)
+ALTER TABLE dbo.DayBookings ADD CONSTRAINT NotNegativeStudents CHECK (NumberOfStudents >= 0)
+ALTER TABLE dbo.WorkshopBookings ADD CONSTRAINT CheckStatusWB CHECK (Status = -1 OR Status = 0 OR Status = 1)
+ALTER TABLE dbo.WorkshopBookings ADD CONSTRAINT PositiveParticipantsWB CHECK (NumberOfParticipants > 0)
