faa6e989f9e624dc1845ceeecb06fc8c0dcbbbf8
AlexMaz99 <ola_m10@onet.pl>
2020-01-19 23:40:30 +0100

    changes

--- constraints.sql
+++ constraints.sql
@@ -1,1 +1,3 @@
-ALTER TABLE dbo.Employees ADD CONSTRAINT EmployeesPK PRIMARY KEY (EmployeeID)
+ALTER TABLE dbo.Employees ADD CONSTRAINT LikeEmail CHECK (Email LIKE'%_@%._%')
+ALTER TABLE dbo.DayPrices ADD CONSTRAINT SDiscount CHECK (StudentDiscount>=0 AND StudentDiscount<=1)
+ALTER TABLE dbo.Days ADD CONSTRAINT PositiveParticipants CHECK (MaxParticipants > 0)
